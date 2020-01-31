f1782ec8b2674cc5ed5f4da5f1233adc1f1878e6
Irving Herrera <irving.herreramolina@gmail.com>
2020-01-31 11:44:36 -0600

    componente de carta

--- frontend/DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ frontend/DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../service/user.service';
-import { AlertifyService } from '../service/AlertifyService';
-import { User } from '../models/User';
+import { UserService } from '../../service/user.service';
+import { AlertifyService } from '../../service/AlertifyService';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-member-list',
