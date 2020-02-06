1d1ec4f8f0073751efa813121160522aa87359e3
Irving Herrera <irving.herreramolina@gmail.com>
2020-02-06 12:29:52 -0600

    se agrego funcionalidad para mostrar alerta cuando se modifica formlario

--- frontend/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ frontend/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRouteSnapshot, ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/User';
+import { NgForm } from '@angular/forms';
+import { AlertifyService } from 'src/app/service/AlertifyService';
 
 @Component({
   selector: 'app-member-edit',
@@ -9,13 +11,20 @@ import { User } from 'src/app/models/User';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
+  @ViewChild('editForm', {static: true}) editForm: NgForm;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private alertifyService: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+  }
+
+  updateUser() {
+    this.alertifyService.success('Profile update successfully');
+    this.editForm.reset(this.user);
   }
 
 }
